bbd6425339fdfd6cd2a163a085ca962179df50d4
Mikolay Bashta <mikolay4401@ukr.net>
2020-01-23 15:46:20 +0200

    Add file main.js

---
+++ main.js
@@ -0,0 +1,79 @@
+function show(data) {
+    console.log(data);
+}
+/////Even numbers
+/*var n = 4;
+
+if(n%2 == 0){
+    show('The number is even');
+}
+
+else{
+    show('The number is not even');
+}
+*/
+
+/// Near numbers
+/*var a = 111.5;
+var b = 101;
+
+if(a< 10 || a>10 && a<b){
+    show(a);
+}
+
+else{
+    show(b);
+}
+*/
+///Cycle
+/*
+for (var i = 1000; i < 1600; i+=3 ) {
+     show(i);
+}
+*/
+
+///Max 50 value
+/*for(var i = 2; i< 90000000000000000; i*=2){
+    if(i == Math.pow (2,50))break;
+    show(i);
+}
+*/
+
+
+///////////////MAKSUMYM/////////////
+/////quadratic equation
+/*var a = 10;
+var b = 24;
+var c = 2;
+var x1,x2,x
+var d = b*b-4*a*c;
+
+
+if(d<0){
+    show('The level has no real roots');
+}
+
+else if (d>0){
+    x1 = -b+Math.sqrt(d)/2*a;
+    x2 = -b-Math.sqrt(d)/2*a;
+    show(x1);
+    show(x2);
+}
+
+else{
+    x = -b/2*a;
+    show (x);
+}
+*/  
+///// Inalienable elements
+/*for( i = 90; i > 0; i-=5 ){
+    show(i);
+}
+*/
+/////the first 20 items
+/*for(i = 2; i < 9000000; i*=2){
+    if(i == Math.pow(2,21)) break;
+    show(i);
+}
+*/
+
